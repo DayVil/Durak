4c1cd8d8b3949c30db74e0cd4c19fa6cb2defa30
rob_manja <rob@manja.com>
2021-06-24 10:41:55 +0200

    changes to login screen controller

--- client/src/main/java/de/uni_hannover/hci/cardgame/Controller/LoginController.java
+++ client/src/main/java/de/uni_hannover/hci/cardgame/Controller/LoginController.java
@@ -2,31 +2,37 @@ package de.uni_hannover.hci.cardgame.Controller;
 
 import de.uni_hannover.hci.cardgame.ControllerInterface;
 import de.uni_hannover.hci.cardgame.fxmlNavigator;
+import de.uni_hannover.hci.cardgame.gameClient;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-// TODO: restructure controller and fxml to use resize function and to implement the init function
 public class LoginController implements ControllerInterface
 {
 
-    @FXML
-    private Button loginButton;
+    @Override
+    public void init ()
+    {
+    }
 
+    @Override
+    public void resize (Number newValue, Boolean isHeight)
+    {
+
+    }
     @FXML
     private void goToLoading()
     {
         fxmlNavigator.loadFxml(fxmlNavigator.LOADING);
     }
 
-    @Override
-    public void resize(Number newValue, Boolean isHeight)
-    {
 
-    }
-
-    @Override
-    public void init()
-    {
-
-    }
 }
