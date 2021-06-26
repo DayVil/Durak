379032543c0665b621a38f279977a472562fec25
rob_manja <robert.witteck@stud.uni-hannover.de>
2021-06-26 12:24:42 +0200

    Client connection to server established

--- client/src/main/java/de/uni_hannover/hci/cardgame/Controller/LoginController.java
+++ client/src/main/java/de/uni_hannover/hci/cardgame/Controller/LoginController.java
@@ -15,8 +15,21 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class LoginController implements ControllerInterface
-{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+import java.net.*;
+import java.io.*;
+
+public class LoginController implements ControllerInterface {
+
+    private Socket clientSocket;
+    private PrintWriter bufferOut;
+    private BufferedReader bufferIn;
 
     @FXML
     private Pane Login;
@@ -43,21 +56,25 @@ public class LoginController implements ControllerInterface
     private Label label;
 
     @FXML
-    private void goToHome()
-    {
+    private void goToHome() {
         fxmlNavigator.loadFxml(fxmlNavigator.HOME);
     }
 
     @FXML
-    private void checkForEntrance()
-    {
+    private void checkForEntrance() {
         //TODO: get this associated with server/client communication, for now just doing the ui with nothing in it
-        fxmlNavigator.loadFxml(fxmlNavigator.GAME);
+//        fxmlNavigator.loadFxml(fxmlNavigator.GAME);
+        System.out.println("Connecting to server " + IPAddress.getCharacters());
+//        System.out.println(UserName.getCharacters());
+//        System.out.println(Password.getCharacters());
+        startConnection("127.0.0.1", 8000);
+        sendMessage("test");
+
+
     }
 
     @Override
-    public void resize (Number newValue, Boolean isHeight)
-    {
+    public void resize(Number newValue, Boolean isHeight) {
         Stage stage = gameClient.stage_;
         Scene scene = stage.getScene();
 
@@ -66,14 +83,11 @@ public class LoginController implements ControllerInterface
 
         if (isHeight) {
             sH = (double) newValue;
-        }
-        else
-        {
+        } else {
             sW = (double) newValue;
         }
 
-        if (sH <= 0.0 || sW <= 0.0)
-        {
+        if (sH <= 0.0 || sW <= 0.0) {
             return;
         }
 
@@ -83,7 +97,7 @@ public class LoginController implements ControllerInterface
 
         Content = (Pane) scene.lookup("#Content");
         Content.setPrefHeight(sH);
-        Content.setPrefWidth(sW/3.0);
+        Content.setPrefWidth(sW / 3.0);
 
         IPAddress = (TextField) scene.lookup("#IPAddress");
         NodeResizer.resizeObject(sW, sH, IPAddress, true);
@@ -108,8 +122,7 @@ public class LoginController implements ControllerInterface
     }
 
     @Override
-    public void init ()
-    {
+    public void init() {
         NodeResizer.originalSceneHeight = 400.0;
         NodeResizer.originalSceneWidth = 600.0;
         Scene scene = gameClient.stage_.getScene();
@@ -119,5 +132,37 @@ public class LoginController implements ControllerInterface
                         pauseFinishedEvent -> resize(scene.getHeight(), true)
                 );
         pause.play();
+    }
+
+
+    public void startConnection(String ip, int port) {
+        try {
+            clientSocket = new Socket(ip, port);
+            bufferOut = new PrintWriter(clientSocket.getOutputStream(), true);
+            bufferIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            System.out.println("error");
+        }
+
+    }
+
+    public String sendMessage(String msg) {
+        try {
+            bufferOut.println(msg);
+            return bufferIn.readLine();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void stopConnection() {
+        try {
+            bufferIn.close();
+            bufferOut.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
     }
 }
